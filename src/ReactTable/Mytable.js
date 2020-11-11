b5fbf639e7318272ad3eb87fb464d914211a9acf
Shorojit1997 <shorojitsarkar1997@gmail.com>
2020-11-12 00:38:50 +0600

    unsuccessfull to create react table

---
+++ src/ReactTable/Mytable.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import ReactTable from 'react-table'
+
+
+
+class Mytable extends Component {
+    render() {
+        const tabledata=[{name:"Sagor",age:"23"}]
+        const col=[{Header:"Name",accessor:""},{Header:"Age",accessor:""}]
+
+        return (
+            <div>
+                <ReactTable>
+                    data={tabledata}
+                    columns={col}
+                    defaultPageSize={2}
+                    pageSizeOptions={[2,4,6,7]}
+                </ReactTable>
+                
+            </div>
+        );
+    }
+}
+
+export default Mytable;
